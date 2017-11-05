c6adfd3dc3079b0693e00395a681b742c52c1e3d
Yamuna Navada <yamunanavada@gmail.com>
2017-11-05 19:23:48 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,12 +1,12 @@
 class Hash
   def keys_of(*arguments)
     # code goes here
-    array = []
+    
     self.collect do |key, value|
       if value == arguments
         array.push(key.to_s)
       end
     end
-    array
+    
   end
 end
