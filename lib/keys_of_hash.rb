835743428b8187be4ec2965671cbe38e62a16591
Yamuna Navada <yamunanavada@gmail.com>
2017-11-05 19:44:46 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,18 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    array = []
+
+
+    arguments.each do |arg|
+      self.each do |key, value|
+        if value == arg
+          array.push(key)
+        end
+      end
+    end
+    array
+
+
   end
 end
